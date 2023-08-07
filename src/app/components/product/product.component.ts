e00e62891954b4bf3ad4d8bbc5c004f31e9c06c3
Süleyman Beyaz <suleymanbeyaz10@gmail.com>
2023-08-07 12:55:13 +0300

    popper.js sorunu çözüldü

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -12,8 +14,12 @@ export class ProductComponent implements OnInit {
 
   products: Product[] = [];
   dataLoaded = false;
+  filterText = "";
 
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { }
+  constructor(private productService: ProductService,
+    private activatedRoute: ActivatedRoute,
+    private toastrService: ToastrService,
+    private cartService: CartService) { }
 
   ngOnInit(): void {
 
@@ -40,5 +46,13 @@ export class ProductComponent implements OnInit {
       this.products = response.data
       this.dataLoaded = true;
     })
+  }
+
+  addToCart(product: Product) {
+    if (product.productId === 1) {
+      this.toastrService.error("Hata (test amaçlı)", "Bu ürün sepete eklenemez.")
+    } else {
+      this.toastrService.success("Sepete eklendi", product.productName)
+    }
   }
 }
